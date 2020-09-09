e3f724a34d4c9fe3f261828df5bbc11efa119fa6
Gordeyev <Woland_s-right-slipper@mail.ru>
2020-09-09 17:39:04 +0300

    refactored

--- app/src/main/java/com/example/testapplication/network/ServiceRepository.kt
+++ app/src/main/java/com/example/testapplication/network/ServiceRepository.kt
@@ -5,13 +5,25 @@ import com.example.testapplication.models.ServiceItem
 import okhttp3.MediaType
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
+import retrofit2.Response
+import timber.log.Timber
 
-class ServiceRepository(private val api: ApiService) : BaseRepository() {
+class ServiceRepository(private val api: ApiService) {
     suspend fun getServices(): List<ServiceItem>? {
-        return safeApiCall(
+        safeApiCall(
                 call = { api.getServiceList() },
                 error = "getServerList error"
-        )
+        ).also {
+            when (it) {
+                is ResponseResult.Success -> {
+                    return it.data
+                }
+                is ResponseResult.Error -> {
+                    return null
+                }
+            }
+        }
+        return null
     }
 
     suspend fun sendForm(form: Form): String? {
@@ -27,9 +39,28 @@ class ServiceRepository(private val api: ApiService) : BaseRepository() {
         safeApiCall(
                 call = { api.sendForm(name, comment, photo) },
                 error = "send form error"
-        )?.let {
-            return it.message
+        ).also {
+            when (it) {
+                is ResponseResult.Success -> {
+                    return it.data?.message
+                }
+                is ResponseResult.Error -> {
+                    return it.message
+                }
+            }
         }
         return null
+    }
+
+    private suspend fun <T : Any> safeApiCall(call: suspend () -> Response<T>, error: String): ResponseResult<T>? {
+        call.invoke().also {
+            return if (it.isSuccessful) {
+                Timber.d("api response successful")
+                ResponseResult.Success(it.body()!!)
+            } else {
+                Timber.d("api response failure. ${it.message()}")
+                ResponseResult.Error(it.message())
+            }
+        }
     }
 }
